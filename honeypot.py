23d85666e147f16555221ea27907192304bd3191
jesse <jesse.mazzie@agile5technologies.com>
2019-03-23 01:16:33 -0400

    Slight changes.

--- honeypot.py
+++ honeypot.py
@@ -1,9 +1,28 @@
 import socket
+import sys
 
 
 
+#Main function. Controls general program flow.
 def main():
-    print('Starting...')
+    if len(sys.argv) != 2:
+        print('No port number supplied.')
+        exit()
+    else:
+        print('Starting...')
+        openPort(sys.argv[1]) #strip first element from sys.argv, because that is the program name.
 
+
+
+def exit():
+    print('Exiting...')
+    sys.exit()
+
+def openPort(port):
+    try:
+        port = int(port)
+        print(port)
+    except TypeError:
+        print('Invalid arguments. Argument must be integer in range: 0-65535.')
 
 main()
