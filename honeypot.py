0044d6131a28a964cb2b987b31cabbc2b77ec585
Jesse Mazzie <jessegalt@yahoo.com>
2019-03-27 19:03:14 -0400

    Added simple logging function.

--- honeypot.py
+++ honeypot.py
@@ -5,39 +5,43 @@ import sys
 
 #Main function. Controls general program flow.
 def main():
-    if len(sys.argv) != 2:
-        print('No port number supplied.')
-        exit()
-    else:
-        print('Starting...')
-        openPort(sys.argv[1]) #strip first element from sys.argv, because that is the program name.
+	if len(sys.argv) != 2:
+		print('No port number supplied.')
+		exit()
+	else:
+		print('Starting...')
+		openPort(sys.argv[1]) #strip first element from sys.argv, because that is the program name.
 
 
 #Exits program
 def exit():
-    print('Exiting...')
-    sys.exit()
+	print('Exiting...')
+	sys.exit()
 
 #Listens on a given port.
 def openPort(port):
-    try:
-	    sock = socket(AF_INET, SOCK_STREAM)
-	    port = int(port)
-	    print(port)
+	try:
+		sock = socket(AF_INET, SOCK_STREAM)
+		port = int(port)
+		print(port)
 	
-	    sock.bind(('127.0.0.1', port))
+		sock.bind(('127.0.0.1', port))
 	
-	    sock.listen()
+		sock.listen()
 		
-	    while True:
-		    connection, client_addr = sock.accept()
 			
-		    print(connection)
+		while True:
+			connection, client_addr = sock.accept()
+			print(connection)
 			
-		    print(client_addr)
+			print(client_addr)
     
-    except TypeError:
-        print('Invalid arguments. Argument must be integer in range: 0-65535.')
+	except TypeError:
+		print('Invalid arguments. Argument must be integer in range: 0-65535.')
+
+def log(text_to_log):
+	with open("honeypot.log") as f:
+		f.write(text_to_log)
 
 #enter main program loop.
 main()
